e908f051ecf240221b44d6af36cf1af7acb2ca3d
Amit Bhagat <78423707+Amitkumarbhagat@users.noreply.github.com>
2024-03-05 13:04:45 +0530

    Add project files.

---
+++ EmployeeService/Controllers/EmployeesController.cs
@@ -0,0 +1,29 @@
+﻿using EmployeeService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeService.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (HRDBContext dbContex = new HRDBContext())
+            {
+                return dbContex.Employees.ToList();
+            }
+        }
+
+        public Employee Get(int id)
+        {
+            using (HRDBContext dbContex = new HRDBContext())
+            {
+                return dbContex.Employees.FirstOrDefault(e => e.id == id);
+            }
+        }
+    }
+}
