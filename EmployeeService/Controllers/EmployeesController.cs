c9b0affbe2b0d16b3663675f0e94e0b68953ac56
Amit Bhagat <78423707+Amitkumarbhagat@users.noreply.github.com>
2024-03-05 13:14:43 +0530

    Created Get method with httpresponseMessage via ms sql server db

--- EmployeeService/Controllers/EmployeesController.cs
+++ EmployeeService/Controllers/EmployeesController.cs
@@ -8,21 +8,48 @@ using System.Web.Http;
 
 namespace EmployeeService.Controllers
 {
+
+    // Get : api/Employees
     public class EmployeesController : ApiController
     {
-        public IEnumerable<Employee> Get()
+        public HttpResponseMessage Get()
         {
+            List<Employee> employees = new List<Employee>();
             using (HRDBContext dbContex = new HRDBContext())
             {
-                return dbContex.Employees.ToList();
+                employees = dbContex.Employees.ToList();
+                if (employees.Count == 0)
+                {
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError, "Please try again later");
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, employees);
+                }
             }
         }
-        //
-        public Employee Get(int id)
+
+        //public IEnumerable<Employee> Get()
+        //{
+        //    using (HRDBContext dbContex = new HRDBContext())
+        //    {
+        //        return dbContex.Employees.ToList();
+        //    }
+        //}
+
+        public HttpResponseMessage Get(int id)
         {
             using (HRDBContext dbContex = new HRDBContext())
             {
-                return dbContex.Employees.FirstOrDefault(e => e.id == id);
+                var emp = dbContex.Employees.FirstOrDefault((e => e.id == id));
+                if (emp != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, emp);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, "Employee with id" + id + "Not found in our database");
+                }
             }
         }
     }
